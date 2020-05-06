78cf463eb4af26c2c919bfba7ce441b95db6460f
LittleBitGirl <whyislam21@gmail.com>
2020-05-06 16:48:24 +0600

    'gnb'

--- bayessian_nlp.py
+++ bayessian_nlp.py
@@ -1,15 +1,13 @@
-import re
 from tkinter import *
 
+import math
+import random
 from sklearn import preprocessing
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import MinMaxScaler
 from collections import Counter
 import codecs
 import string, sys, array
-import numpy as nm
 import pandas as pd
 
 
@@ -114,11 +112,87 @@ def getAccuracy(word, corrected):
     levenshtein = minimumEditDistance(word, corrected)
     return (1 - levenshtein / len(corrected)) * 100
 
+def loadDataset():
+    dataset = pd.read_excel('dataset_x_y.xlsx', index_col=0).to_dict()
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if vector[-1] not in separated:
+            separated[vector[-1]] = []
+            separated[vector[-1]].append(vector)
+    return separated
+
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    return math.sqrt(variance)
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1/(math.sqrt(2*math.pi)*stdev))*exponent
+
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
 
 def normalize(textPath):
-    text = codecs.open(textPath, 'r', 'utf_8')
-    wordsArray = tokenize(text)
-    normalizedArray = []
+    dataset = loadDataset()
+    X_Y_splitted_dataset = splitDataset(dataset, 0.8)
+    summary = summarizeByClass(X_Y_splitted_dataset)
+
     overall_accuracy = 0
     count = 0
     corrected_count = 0
@@ -157,7 +231,7 @@ def normalize(textPath):
 
 
 if __name__ == "__main__":
-    X_Y_dict = pd.read_excel('dataset_x_y.xlsx', index_col=0).to_dict()
+    # X_Y_dict = pd.read_excel('dataset_x_y.xlsx', index_col=0).to_dict()
     X = list(X_Y_dict['Y'].keys())
     Y = list(X_Y_dict['Y'].values())
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
@@ -173,7 +247,9 @@ if __name__ == "__main__":
     X_test = enc.transform(X_test).toarray()
 
     gnb = GaussianNB()
+    gnb.
     y_pred = gnb.fit(X_train, y_train)
+
     print('Accuracy of GNB classifier on training set: {:.2f}'
           .format(gnb.score(X_train, y_train)))
     print('Accuracy of GNB classifier on test set: {:.2f}'
